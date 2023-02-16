ea0fcbc7a75e623f73b5ce56fda334f30115a9a8
zborovskaya <ania.zborovskaya@gmail.com>
2023-02-16 22:23:17 +0300

    ajax2

---
+++ WebApplication2/WebApplication2/WebForm1.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+     <form id="form1" runat="server">
+    <div>
+       <h4>Select a file to upload:</h4>
+   
+       <asp:FileUpload id="FileUpload1"                 
+           runat="server">
+       </asp:FileUpload>
+            
+       <br /><br />
+       
+       <asp:Button id="UploadButton" 
+           Text="Upload file"
+           OnClick="UploadButton_Click"
+           runat="server">
+       </asp:Button>    
+       
+       <hr />
+       
+       <asp:Label id="UploadStatusLabel"
+           runat="server">
+       </asp:Label> 
+        <br>
+        <asp:Image id="Image1" runat="server"
+           AlternateText="Image text"
+           ImageAlign="left"
+           ImageUrl="images/isewtweetbg.jpg" Height="500px" Width="600px"/>       
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            // Specify the path on the server to
+            // save the uploaded file to.
+            String savePath = @"c:\users\zav\documents\visual studio 2015\Projects\WebApplication2\WebApplication2\images\";
+
+            // Before attempting to perform operations
+            // on the file, verify that the FileUpload 
+            // control contains a file.
+            if (FileUpload1.HasFile)
+            {
+                // Get the name of the file to upload.
+                String fileName = FileUpload1.FileName;
+
+                // Append the name of the file to upload to the path.
+                savePath += fileName;
+
+
+                // Call the SaveAs method to save the 
+                // uploaded file to the specified path.
+                // This example does not perform all
+                // the necessary error checking.               
+                // If a file with the same name
+                // already exists in the specified path,  
+                // the uploaded file overwrites it.
+                FileUpload1.SaveAs(savePath);
+
+                // Notify the user of the name of the file
+                // was saved under.
+                UploadStatusLabel.Text = "Your file was saved as " + fileName;
+                Image1.ImageUrl = "images/"+fileName;
+            }
+            else
+            {
+                // Notify the user that a file was not uploaded.
+                UploadStatusLabel.Text = "You did not specify a file to upload.";
+            }
+
+        }
+    }
+}
